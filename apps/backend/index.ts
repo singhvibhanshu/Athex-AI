cbb0e3f3b7edb9cfd5d38eb71815c4e061bf0dc5
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-09-23 20:30:47 +0530

    bun init done in types and basic endpoints are defined

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.post
+
+app.post
+
+app.post
+
+app.get
+
+app.get
