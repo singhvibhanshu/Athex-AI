37513395e7b731ca95199f14cfc5de441139388d
singhvibhanshu <singhvibhanshu@hotmail.com>
2025-09-25 00:45:44 +0530

    imported ./types

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { createNewAIModel, GenerateImages, GenerateImagesFromPack } from "types/types";
+import { prismaClient } from "db";
+import "./types";
 
 const PORT = process.env.PORT || 8080;
 
